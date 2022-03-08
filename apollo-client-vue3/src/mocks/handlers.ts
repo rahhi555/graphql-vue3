09570d6670390da099b7c07eea983d265fd3cb52
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-03-09 06:57:48 +0900

    vitest側のディレクトリ削除

--- apollo-client-vue3/src/mocks/handlers.ts
+++
@@ -1,7 +0,0 @@
-import { graphql } from 'msw'
-
-export const handlers = [
-  graphql.query('useAllPostsQuery', (_, res, ctx) => {
-    return res(ctx.data({ posts: { id: 'hoge', title: 'hoge', author: 'hoge' } }))
-  })
-]
--- apollo-client-vue3/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import PostVue from '@/components/posts/Post.vue';
-</script>
-
-<template>
-  <main>
-    <PostVue />
-  </main>
-</template>
