78b7dd4becdab605edda30195a678e3d070b8724
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-03-08 23:19:38 +0900

    commit

--- server/app/graphql/types/mutation_type.rb
+++ server/app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :login, mutation: Mutations::Login
     field :user_create, mutation: Mutations::UserCreate
     field :post_delete, mutation: Mutations::PostDelete
     field :post_update, mutation: Mutations::PostUpdate
--- server/spec/channels/graphql_channel_spec.rb
+++ server/spec/channels/graphql_channel_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe GraphqlChannel, type: :channel do
+  include RSpec::Rails::RequestExampleGroup
+
   it 'successfully subscribes' do
     subscribe
     expect(subscription).to be_confirmed
